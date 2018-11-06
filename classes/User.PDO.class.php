8219fd418dac280063f3d891eb4c19732aee5bc4
Anna Jacobsen <aj4693@rit.edu>
2018-11-06 10:27:03 -0500

    Added comments to User.PDO

--- classes/User.PDO.class.php
+++ classes/User.PDO.class.php
@@ -1,9 +1,17 @@
 <?php 
     require_once "PDO.DB.class.php";
     include "User.class.php";
+	/*
+	* UserDB class contains all of the methods for using PHP Data Objects to 
+	* interface with the database, specifically in relation to users.
+	* version 11/6/2018
+	*/
     class UserDB extends DB{
         private $dbConn;
-
+		/*
+		* Constructor for UserDB calls the parent constructor and obtains the connection
+		* using the connection accessor method. This will allow us to use methods in the parent class.
+		*/
         function __construct(){
             parent::__construct();
             $this->dbConn = $this->getConn();
