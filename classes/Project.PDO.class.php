b6f485fb71818ff1a9b037d8140f10873982e203
Anna Jacobsen <aj4693@rit.edu>
2018-11-06 10:24:56 -0500

    Added comments to Project.PDO

--- classes/Project.PDO.class.php
+++ classes/Project.PDO.class.php
@@ -1,9 +1,18 @@
 <?php 
     require_once "PDO.DB.class.php";
     include "Project.class.php";
+	/*
+	* ProjectDB class contains all of the methods for using PHP Data Objects to 
+	* interface with the database.
+	* version 11/6/2018
+	*/
     class ProjectDB extends DB{
         private $dbConn;
 
+		/*
+		* Constructor for Project PDO calls the parent constructor and obtains the connection
+		* using the connection accessor method. This will allow us to use methods in the parent class.
+		*/
         function __construct(){
             parent::__construct();
             $this->dbConn = $this->getConn();
