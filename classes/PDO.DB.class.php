3382d09db894949e60825ccc2ae00f84342f89f7
Anna Jacobsen <aj4693@rit.edu>
2018-11-08 17:46:12 -0500

    Filling in method bodies for PDO.DB
    Filled method bodies in for the following:
    - updateSingleField
    - delete
    - getFieldById

--- classes/PDO.DB.class.php
+++ classes/PDO.DB.class.php
@@ -1,14 +1,14 @@
 <?php
     /*
-    * DB class contains all of the methods for creating a connection to,
-    * making an object for, and interacting with the Database.
-	* version 11/6/2018
+    * DB class contains all generalized methods for creating a connection to,
+    * retrieving from, updating, deleting from, and inserting into the database.
+	* version 11/8/2018
 	*/
     class DB{
         private $dbh;
 
         /**
-         * __construct() - creates a new PDO database object, opening a connection at the same time
+         * __construct() - creates a new PDO database object and opens a connection.
          */
         function __construct(){
             try{
@@ -33,24 +33,67 @@
         }
 
         /**
-         * updateField() - updates an item for any field for any table
+         * updateField() - updates a column for any field for any table
          */
         function updateField($tableName, $fieldName, $value, $id){
-            
+            $ra = -1;
+            try{
+                $query = "UPDATE :table SET :column = :value WHERE id = :id";
+                $stmt = $this->dbh->prepare($query);
+                $stmt->bindParam(array(
+                    ":table"=>$tableName,
+                    ":column"=>$fieldName,
+                    ":value"=>$value,
+                    ":id"=>$id
+                ));
+                $ra = $stmt->execute();
+            }catch(PDOException $e){
+                return "A problem occurred updating $tableName";
+            }
+            return $ra;
         }
 
         /**
          * delete() - deletes any entry for any table
          */
         function delete($tableName, $id){
-
+            $ra = -1;
+            try{
+                $query = "DELETE FROM :tableName WHERE id = :id";
+                $stmt = $this->dbh->prepare($query);
+                $stmt->bindParam(array(
+                    ":table"=>$tableName,
+                    ":id"=>$id
+                ));
+                $ra = $stmt->execute();
+            }catch(PDOException $e){
+                return "A problem occurred deleting from $tableName";
+            }
+            return $ra;
         }
 
         /**
          * getFieldById() - Returns a specific entry from any table 
          */
         function getFieldById($tableName, $fieldName, $id){
-
+            $data = array();
+            try{
+                $query = "SELECT :fieldName FROM :table WHERE id = :id";
+                $stmt = $this->dbh->prepare($query);
+                $stmt->setFetchMode(PDO::FETCH_ASSOC);
+                $stmt->bindParam(array(
+                    ":table"=>$tableName,
+                    ":column"=>$fieldName,
+                    ":id"=>$id
+                ));
+                $stmt->execute();
+                while($item = $stmt->fetch()){
+                    $data[] = $item;
+                }
+            }catch(PDOException $e){
+                return "A problem occurred selecting the $fieldName from the table $tableName";
+            }
+            return $data;
         }
 
         /**
