17e89c459e04b7a762984a200ff2f566149e8c20
Dale Moore <dmoore092@gmail.com>
2018-11-17 13:16:43 -0500

    fixed syntax error in user.pdo

--- AbbeyTest.php
+++ AbbeyTest.php
@@ -1,6 +1,8 @@
 <?php
-    require_once('classes/project.PDO.class.php');
+    require_once('classes/Project.PDO.class.php');
     require_once('classes/User.PDO.class.php');
+   
+
     $db = new DB;
     $projectID = $db->getProjectbyId(1);
     var_dump($projectID);  
