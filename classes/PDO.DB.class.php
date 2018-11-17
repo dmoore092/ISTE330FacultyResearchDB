c26503ad56f305ac2472e6204262a31a604c6baf
Dale Moore <dmoore092@gmail.com>
2018-11-17 13:57:10 -0500

    fix dbh to dbconn

--- classes/PDO.DB.class.php
+++ classes/PDO.DB.class.php
@@ -13,11 +13,11 @@
         function __construct(){
             try{
                 // open a connection
-                $this->dbh = new PDO("mysql:host={$_SERVER['DB_SERVER']};dbname={$_SERVER['DB']}",
+                $this->dbconn = new PDO("mysql:host={$_SERVER['DB_SERVER']};dbname={$_SERVER['DB']}",
                     $_SERVER['DB_USER'],$_SERVER['DB_PASSWORD']);
 
                 // Change the error reporting for development
-                $this->dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+                $this->dbconn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
             }
             catch(PDOException $e){
