78fc910bfd1be62e4f11c437a205b3caf22383d9
Jake O'Connor <jo8390@rit.edu>
2018-11-20 16:38:21 -0500

    title style to be below nav

--- assets/inc/nav.php
+++ assets/inc/nav.php
@@ -2,11 +2,11 @@
 ?>
 <!-- NAVIGATION-->
 <nav class="hover-underline-menu" data-menu-underline-from-center>
-      <ul class="menu align-center">
-          <li><a href="index.php">Home</a></li>
-          <li><a href="myProjects.php">My Projects</a></li>
-          <li><a href="findProject.php">Find Projects</a></li>
-          <li><a href="myInfo.php">My Info</a></li>
-          <li><a href="login.php">Login</a></li>
-      </ul>
-  </nav>
+    <ul class="menu align-center">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="myProjects.php">My Projects</a></li>
+        <li><a href="findProject.php">Find Projects</a></li>
+        <li><a href="myInfo.php">My Info</a></li>
+        <li><a href="login.php">Login</a></li>
+    </ul>
+</nav>
