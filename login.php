34d0a8bb255decf2770f7cf52acfca25510c4978
Dale Moore <dmoore092@gmail.com>
2018-11-27 12:51:21 -0500

    fixed vertical positioning on the login page

--- login.php
+++ login.php
@@ -24,27 +24,27 @@
   }
 
 ?>
-
-<div id="feedback">
-  <div data-closable class="callout alert-callout-subtle alert">
-    <strong>ERROR:</strong> Username or password are incorrect
-      <button class="close-button" aria-label="Dismiss alert" type="button" data-close>
-        <span aria-hidden="true">⊗</span>
-    </button>
-  </div>
-</div>
-<form class="loginForm" action="login.php" method="POST">
-  <div class="sign-in-form">
-    <label for="sign-in-form-username">Username</label>
-    <input type="text" class="sign-in-form-username" id="sign-in-form-username" name="username">
-    <label for="sign-in-form-password">Password</label>
-    <input type="password" class="sign-in-form-password" id="sign-in-form-password" name="password">
-    <input type="submit" class="sign-in-form-button" value="Login" name="login"/> 
-    <p class="text-center"><a href="#">Forgot your password?</a></p>
-    <p class="text-center"><a href="#">Create Account</a></p>
-  </div>
-</form>
-
+<main>
+    <div id="feedback">
+        <div data-closable class="callout alert-callout-subtle alert">
+            <strong>ERROR:</strong> Username or password are incorrect
+            <button class="close-button" aria-label="Dismiss alert" type="button" data-close>
+                <span aria-hidden="true">⊗</span>
+            </button>
+        </div>
+    </div>
+    <form class="loginForm" action="login.php" method="POST">
+        <div class="sign-in-form">
+            <label for="sign-in-form-username">Username</label>
+            <input type="text" class="sign-in-form-username" id="sign-in-form-username" name="username">
+            <label for="sign-in-form-password">Password</label>
+            <input type="password" class="sign-in-form-password" id="sign-in-form-password" name="password">
+            <input type="submit" class="sign-in-form-button" value="Login" name="login"/> 
+            <p class="text-center"><a href="#">Forgot your password?</a></p>
+            <p class="text-center"><a href="#">Create Account</a></p>
+        </div>
+    </form>
+</main>
 <?php
   include_once 'assets/inc/validation.php';
   include_once 'assets/inc/footer.php';
