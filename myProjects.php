f59500a1369c8779a5a6c132f85ad916f4d30550
Anna Jacobsen <aj4693@rit.edu>
2018-11-27 10:10:07 -0500

    Wrote the script for generating the content on the my projects page

--- myProjects.php
+++ myProjects.php
@@ -1,4 +1,26 @@
 <?php
+
+session_name("user");
+  session_start();
+  include_once "assets/inc/nav.php";
+  include_once "classes/Project.PDO.class.php";
+  $projectDB = new ProjectDB();
+  echo "<h1 class='title'>Find Projects</h1>";
+  if(isset($_SESSION['loggedIn']) && $_SESSION['loggedIn']){
+    switch($_SESSION['role']){
+      case 1:
+        echo "<h1>I am a professor.</h1>";
+        echo $projectDB->getProjectsAsTable(true);
+      break;
+      case 2:
+        echo "<h1>I am a student.</h1>";
+        echo $projectDB->getProjectsAsTable();
+      break;
+    }
+  }else{
+   echo "<p>You must be logged in to see your projects.<p><br/><a href='login.php'>Login</a>";
+  }
+
 ?>
 <!DOCTYPE HTML>
 <html>
