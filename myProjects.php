ca356819824c42549466becdd867b8a1dc6d60e6
Anna Jacobsen <aj4693@rit.edu>
2018-11-27 10:51:26 -0500

    Working on getting the project table displaying

--- myProjects.php
+++ myProjects.php
@@ -1,11 +1,13 @@
 <?php
 
+include_once 'assets/inc/header.php';
+
 session_name("user");
   session_start();
   include_once "assets/inc/nav.php";
   include_once "classes/Project.PDO.class.php";
   $projectDB = new ProjectDB();
-  echo "<h1 class='title'>Find Projects</h1>";
+  echo "<h1 class='title'>My Projects</h1>";
   if(isset($_SESSION['loggedIn']) && $_SESSION['loggedIn']){
     switch($_SESSION['role']){
       case 1:
@@ -21,23 +23,5 @@ session_name("user");
    echo "<p>You must be logged in to see your projects.<p><br/><a href='login.php'>Login</a>";
   }
 
-?>
-<!DOCTYPE HTML>
-<html>
-<head>
-  <title>My Projects</title>
-  <?php
-    include_once 'assets/inc/header.php';
-  ?>
-</head>
-<body>
-    <?php
-        include_once 'assets/inc/nav.php';
-    ?>
-<h1 class="title">My Projects</h1>
-
-<?php
   include_once 'assets/inc/footer.php';
 ?>
-</body>
-</html>
