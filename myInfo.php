7fc7fc4322db9ce354e29661dc352d5e89350948
Abbey Sands <als6301@g.rit.edu>
2018-11-27 11:33:25 -0500

    changed position

--- myInfo.php
+++ myInfo.php
@@ -5,16 +5,16 @@
   <title>My Information</title>
   <?php
     include_once 'assets/inc/header.php';
-    $DB = new DB();
-    if (isset($_GET['logout'])) {
-      $DB->logout();
-    }
   ?>
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <?php
         include_once 'assets/inc/nav.php';
+        $DB = new DB();
+        if (isset($_GET['logout'])) {
+          $DB->logout();
+        }
     ?>
 <h1 class="title">My Information</h1>
 
