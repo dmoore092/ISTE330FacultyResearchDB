22239ec40958798d5e4ee98a4d8eaf1392f1439c
Abbey Sands <als6301@g.rit.edu>
2018-11-29 18:17:49 -0500

    Added session variable for ID

--- classes/User.PDO.class.php
+++ classes/User.PDO.class.php
@@ -94,7 +94,7 @@
 		function login($username, $password){
 				$data = array();
 				$hashed_password = hash('sha256', $password);
-				$stmt = $this->dbConn->prepare("select username, password, role from user where username = ?"); 
+				$stmt = $this->dbConn->prepare("select username, password, role, id from user where username = ?"); 
 				$stmt->bindParam(1, $username, PDO::PARAM_STR);
 				$stmt->execute();
 				$stmt->setFetchMode(PDO::FETCH_CLASS,"User");
@@ -110,7 +110,8 @@
 						$_SESSION['username'] = $user->getUsername();
 						$_SESSION['role'] = $user->getRole();
 						$_SESSION['loggedIn'] = true;
-                        $_SESSION['fullname'] = $user->getName();
+						$_SESSION['fullname'] = $user->getName();
+						$_SESSION['id'] = $user->getId();
 					}
 					//true
 					return 1;
