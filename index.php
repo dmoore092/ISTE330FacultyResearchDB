5ffb3d1628eafd35851c782880bb40bdd20364c1
Dale Moore <dmoore092@gmail.com>
2018-11-30 09:06:26 -0500

    working on adding links and text to index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
                 Here, you'll be able to find ongoing projects that IST Professors create, lead, and guide collaborating students toward achieving the final product. Projects can be found by going to the <a href="findProject.php">Find Projects</a> page and browsing through the list of current projects. There, you will be able to see the Professor leading the project, their contact information, and some details about the project.
             </p>
             <p>
-                If you are a student, you can <a href="login.php">login</a> and have the ability to not only search for these projects, but also contact the professor leading the project to join the group. You will see other students that are already members. Once you are part of a project, you will be able to view all the projects you are affiliated with by going to <a href="myProjects.php">My Projects</a>. You can also view your information by going to<a href="myInfo.php">My Information</a> as well as edit anything that needs to be updated.</p>
+                If you are a student, you can <a href="login.php">login</a> and have the ability to not only search for these projects, but also contact the professor leading the project to join the group. You will see other students that are already members. Once you are part of a project, you will be able to view all the projects you are affiliated with by going to <a href="myProjects.php">My Projects</a>. You can also view your information by going to<a href="myInfo.php"> My Information</a> as well as edit anything that needs to be updated.</p>
             <p>
                 Professors manage all of their own projects with the ability to add, edit, and delete anything within the project or the project itself.
             </p>
@@ -27,11 +27,11 @@
         <aside>
             <h3>Research Highlights</h3>
             <a href="findProject.php" ><img src="assets/media/images/research1.jpg"></a>
-            <a href="findProject.php" ><div>IT Fluency in Non-human Primates</div></a>
+            <a href="findProject.php" ><div class="appear">IT Fluency in Non-human Primates</div></a>
             <a href="findProject.php" ><img src="assets/media/images/research2.jpg"></a>
-            <a href="findProject.php" ><div>SOAP: not just for cleaning anymore</div></a>
+            <a href="findProject.php" ><div class="appear">SOAP: not just for cleaning anymore</div></a>
             <a href="findProject.php" ><img src="assets/media/images/research3.jpg"></a>
-            <a href="findProject.php" ><div>Java or PHP? Lets see that back end</div>
+            <a href="findProject.php" ><div class="appear">Java or PHP? Lets see that back end</div>
         </aside>
     </main>
 <?php include_once 'assets/inc/footer.php'; ?>
