4eaf5af2ed8dcffa6306d8d8cfdd5d36bd32aa4c
ISTE330 Team Mongoose <iste330t01@ist-serenity.main.ad.rit.edu>
2018-11-30 17:28:44 -0500

    pushed databases changes

--- database/getMyProjectsSP.sql
+++ database/getMyProjectsSP.sql
@@ -2,7 +2,7 @@ DELIMITER //
 CREATE PROCEDURE getMyProjects
 (IN userId VARCHAR(50))
 BEGIN
-SELECT DISTINCT projectName, name, email, projectDescription, u.id
+SELECT DISTINCT projectName, name AS 'projectLead', email, projectDescription, u.id
 FROM project p JOIN user u
 WHERE p.projectLead = u.id
 AND u.id = userId;
