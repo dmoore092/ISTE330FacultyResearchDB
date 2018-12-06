8523ca79d5b512deb3550839be4d735aff10a3f2
Anna Jacobsen <aj4693@rit.edu>
2018-12-06 17:47:33 -0500

    Update Project.PDO.class.php

--- classes/Project.PDO.class.php
+++ classes/Project.PDO.class.php
@@ -190,7 +190,7 @@
 		try{
                 	$data = array();
                		$stmt = $this->dbConn->prepare("select * from project where projectName LIKE %:name%"); 
-                	$stmt->bindParam(":name",$name,PDO::PARAM_STR, 150);    
+                	$stmt->bindParam(":name", $name, PDO::PARAM_STR, 150);    
                 	$stmt->execute();
                 	$stmt->setFetchMode(PDO::FETCH_CLASS,"Project");
                 	while($databaseProjects = $stmt->fetch()){
